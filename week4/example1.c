4959638788a897ea0a5d0c910b6e0497a70270ab
Kalidu De Costa <kalindu.decosta@mail.utoronto.ca>
2019-02-04 18:20:18 -0500

    week 4 example

---
+++ week4/example1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+// make a struct to store music record information
+
+void create_record(/* take in a pointer to the struct */) {
+
+    // ask for user input to fill in the record
+    
+}
+
+void print_record(/* take in a pointer to the struct */) {
+
+    // print our all the information of the record
+}
+
+int main() {
+
+    // create an array of records
+
+    // populate each record using create record
+
+    // print out each record
+
+}
