83a66a6c2f08bbe53a4b60ec40cc35e42e792cbd
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2019-02-10 18:02:51 -0500

    memory allocation

---
+++ week5/example1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int * newArray(int size) {
+
+    // allocate memory of size 'size' and return an int pointer
+    
+}
+
+int * new2DArray(int size_x, int size_y) {
+
+    // allocate memoru of size ____ and return an int pointer
+}
+
+int main() {
+
+    // create a new array
+    
+
+}
